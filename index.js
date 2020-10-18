af9887dd079664ae392262651b23b4455fcc18e8
Nastaran <nastaranashouri77@gmail.com>
2020-10-18 09:46:44 +0330

    change index file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 export default function stringToReactElement(text, regexAndRendererPairs) {
   const regex = new RegExp(regexAndRendererPairs.map(item => item.regex.source).join('|'), 'gui');
   const splitedText = text.split(regex).filter(item => item !== undefined);
-  const newText = splitedText.map(splitItem => regexAndRendererPairs
-    .find(regexAndRendererPairsItem => regexAndRendererPairsItem.regex.test(splitItem))?.renderer(splitItem));
+  const newText = splitedText.map(splitItem => regexAndRendererPairs.find(regexAndRendererPairsItem => regexAndRendererPairsItem.regex.test(splitItem))?.renderer(splitItem));
   return newText;
 }
