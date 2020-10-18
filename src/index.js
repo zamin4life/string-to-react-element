0d6e469f62e0ec7c9c174e493cdfb5e2265afa4e
Nastaran <nastaranashouri77@gmail.com>
2020-10-18 12:14:47 +0330

    handle undefiend in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 export default function stringToReactElement(text, regexAndRendererPairs) {
   const regex = new RegExp(regexAndRendererPairs.map(item => item.regex.source).join('|'), 'gui');
   const splitedText = text.split(regex).filter(item => item !== undefined);
-  const newText = splitedText.map(splitItem => regexAndRendererPairs
-    .find(regexAndRendererPairsItem => regexAndRendererPairsItem.regex.test(splitItem)
-    && regexAndRendererPairsItem.regex.test(splitItem)).renderer(splitItem));
+  const newText = splitedText.map((splitItem) => {
+    const matchItem = regexAndRendererPairs
+      .find(regexAndRendererPairsItem => regexAndRendererPairsItem.regex.test(splitItem));
+    return matchItem && matchItem.renderer(splitItem);
+  });
   return newText;
 }
